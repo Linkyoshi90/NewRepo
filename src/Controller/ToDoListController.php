4d74195a5760278d806cde6906059a3741e9d9c7
Linkjosha <linkjosha@yahoo.de>
2020-09-29 15:08:15 +0200

    Edit some things
    Functions for basic sorting and duplicate check functionality coded, not yet implemented though

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -13,8 +13,25 @@ class ToDoListController extends AbstractController
      * @Route("/", name="to_do_list")
      */
     public function index() {
-        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([],['id'=>'DESC']);
-        return $this->render('index.html.twig', ['tasks'=>$tasks]);
+        /*
+        * I want to define the order in a method in the future, 
+        */
+        $order = 0;
+        if (!$order) {
+            $order = 'DESC';
+        }
+        $repo = $this->getDoctrine()
+            ->getRepository(Task::class);
+        /* 
+        * Query searches for status and id in Task
+        */
+        $query = $repo->createQueryBuilder('t')
+            ->orderBy('t.status', 'ASC', 't.id', $order)
+            ->getQuery();
+        if (!$query) {
+            throw $this->createNotFoundException('Not found...');
+        }
+        return $this->render('index.html.twig', ['tasks'=>$query->execute()]);
     }
     
     /**
@@ -25,6 +42,27 @@ class ToDoListController extends AbstractController
         if (empty($title)) {
             return $this->redirectToRoute('to_do_list');
         }
+        /*
+        This code throws errors, so I commented it out
+        $whereStatement = 't.title = ' . $title;
+        $repo = $this->getDoctrine()
+            ->getRepository(Task::class);
+        $query = $repo->createQueryBuilder('t')
+            ->where('t.title = :title')
+            ->setParameter('title', $title)
+            ->getQuery();
+        $queryResult = $query->execute();
+        if ($queryResult != $title) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $task = new Task;
+            $task->setTitle($title);
+            $entityManager->persist($task);
+            $entityManager->flush();
+        }
+        else {
+            throw Exception("Already exists");
+        }
+        */
         $entityManager = $this->getDoctrine()->getManager();
         $task = new Task;
         $task->setTitle($title);
@@ -45,12 +83,31 @@ class ToDoListController extends AbstractController
     }
 
     /**
+     * @Route("/order-switch", name="order_switch")
+     */
+    public function orderSwitch() {
+        if ($order == 'DESC') {
+            $order = 'ASC';
+        }
+        else {
+            $order = 'DESC';
+        }
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    /**
      * @Route("/delete/{id}", name="task_delete")
      */
-    public function delete(Task $id) {
+    public function delete(int $id) {
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($id);
-        $entityManager->flush();
+        $repo = $this->getDoctrine()
+            ->getRepository(Task::class);
+        $query = $repo->createQueryBuilder('t')
+            ->delete('Task', 't')
+            ->where('t.id = :id')
+            ->setParameter('id', $id);
+        $query->execute();
+        // Attempted to call an undefined method named "execute" of class "Doctrine\ORM\QueryBuilder".
         return $this->redirectToRoute('to_do_list');
     }
 }
